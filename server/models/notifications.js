010fe26916284c2daf2764ca45e0208124f49834
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2025-03-18 17:08:03 +0300

    Add Notification model and migration

---
+++ server/models/notifications.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./initializer");
+
+const Notification = sequelize.define(
+  "notifications",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users", // Reference to the users table
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    message: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM("message", "friend_request", "group_invite"),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("unread", "read"),
+      defaultValue: "unread",
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false, // Avoid Sequelize's default timestamps
+  }
+);
+Notification.sync()
+  .then(() => {
+    console.log("notification sync success");
+  })
+  .catch(() => {
+    console.log("notification sync failed");
+  });
+
+module.exports = Notification;
